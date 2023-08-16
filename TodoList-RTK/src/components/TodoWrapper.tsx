5391754bbcaff7a1a5a113c7d54d8d8a958af341
Av808080 <108837984+Av808080@users.noreply.github.com>
2023-08-16 10:34:47 +0330

    Filter Todos

--- TodoList-RTK/src/components/TodoWrapper.tsx
+++ TodoList-RTK/src/components/TodoWrapper.tsx
@@ -1,12 +1,23 @@
+import { useState, ChangeEvent } from 'react'
 import TodoItem from "./TodoItem"
 import { useSelector } from 'react-redux'
 import { Todo } from "../Types/Todo"
 const TodoWrapper = () => {
     const todos = useSelector((store: any) => store.todo)
-  
+    const [filterBy, setFilterBy] = useState('همه')
     return (
-        <div>
-            {todos.map(((item: Todo) => <TodoItem key={item.id} todo={item} />))}
+        <div >
+            <select className='bg-teal-300 focus:outline-none ring-2 ring-teal-400 rounded-lg block mx-auto mb-8 mt-4'
+            onChange={(e: ChangeEvent<HTMLSelectElement>) => setFilterBy(e.target.value)}
+            >
+                <option value='All' >همه</option>
+                <option value='Completed' >تکمیل شده</option>
+                <option value='Uncompleted' >تکمیل نشده</option>
+            </select>
+            {filterBy === 'Completed' ? todos.map((item: Todo) => item.isCompleted && <TodoItem key={item.id} todo={item} />) :
+                filterBy === 'Uncompleted' ? todos.map((item: Todo) => !item.isCompleted && <TodoItem key={item.id} todo={item} />) :
+                    todos.map(((item: Todo) => <TodoItem key={item.id} todo={item} />))
+            }
         </div>
     )
 }
